b5727bf2326b3959e9a06473841672fab8eb59fc
Tomasz Pernal <tompern1@googlemail.com>
2022-12-05 10:04:41 +0000

    Add class

--- js/script.js
+++ js/script.js
@@ -4,16 +4,19 @@ function titleClickHandler(event){
   console.log('Link was clicked!');
   console.log(event);
 
-  /* remove class 'active' from all article links  */
+  /* [DONE] remove class 'active' from all article links  */
+
   const activeLinks = document.querySelectorAll('.titles a.active');
 
     for (let activeLink of activeLinks) {
         activeLink.classList.remove('active');
     }
 
-  /* add class 'active' to the clicked link */
+  /* [IN PROGRESS] add class 'active' to the clicked link */
+  clickedElement.classList.add('active');
+  console.log('clickedElement:', clickedElement);
 
-  /* remove class 'active' from all articles */
+  /* [DONE] remove class 'active' from all articles */
   const activeArticles = document.querySelectorAll('.post');
 
     for (let activeArticle of activeArticles) {
