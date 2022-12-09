dc1a6e2ec5a1bde364770c7087f37b8b067a3b19
Tomasz Pernal <tompern1@googlemail.com>
2022-12-09 14:36:19 +0000

    remove links and added html code

--- js/script.js
+++ js/script.js
@@ -37,3 +37,38 @@ const links = document.querySelectorAll('.titles a');
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
 }
+
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+  /* remove contents of titleList */
+  /* ... */
+
+  /* find all the articles and save them to variable: articles */
+  /* ... */
+
+  let html = '';
+
+  for(let article of articles){
+    /* get the article id */
+    /* ... */
+
+    /* find the title element */
+    /* ... */
+
+    /* get the title from the title element */
+    /* ... */
+
+    /* create HTML of the link */
+    /* ... */
+
+    /* insert link into html variable */
+    html = html + linkHTML;
+  }
+
+  titleList.innerHTML = html;
+}
+
+generateTitleLinks();
