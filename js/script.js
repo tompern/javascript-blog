5233c5732e284d4d44a0b394f411d36283f6dc71
Tomasz Pernal <tompern1@googlemail.com>
2022-12-09 14:15:19 +0000

    removed console.log

--- js/script.js
+++ js/script.js
@@ -3,35 +3,30 @@
 function titleClickHandler(event){
     event.preventDefault();
     const clickedElement = this;
-    console.log('Link was clicked!');
-    console.log(event);
 
   /* [DONE] remove class 'active' from all article links  */
 
   const activeLinks = document.querySelectorAll('.titles a.active');
 
-    for (let activeLink of activeLinks) {
+  for (let activeLink of activeLinks) {
         activeLink.classList.remove('active');
-    }
+  }
 
   /* [IN PROGRESS] add class 'active' to the clicked link */
   clickedElement.classList.add('active');
-  console.log('clickedElement:', clickedElement);
 
   /* [DONE] remove class 'active' from all articles */
   const activeArticles = document.querySelectorAll('.post');
 
-    for (let activeArticle of activeArticles) {
+  for (let activeArticle of activeArticles) {
         activeArticle.classList.remove('active');
-    }
+  }
 
   /* [DONE] get 'href' attribute from the clicked link */
   const articleSelector = clickedElement.getAttribute('href');
-  console.log('articleSelector', articleSelector);
 
   /* [DONE] find the correct article using the selector (value of 'href' attribute) */
   const targetArticle = document.querySelector(articleSelector);
-  console.log('targetArticle', targetArticle);
 
   /* [DONE] add class 'active' to the correct article */
   targetArticle.classList.add('active');
